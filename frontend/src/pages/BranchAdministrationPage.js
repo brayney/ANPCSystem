f0f0d99021162a72304fe9f057e879c9d79c238a
brayney <bryneborinaga15@gmail.com>
2026-07-30 20:17:02 +0800

    errors fixed

--- frontend/src/pages/BranchAdministrationPage.js
+++ frontend/src/pages/BranchAdministrationPage.js
@@ -1,4 +1,4 @@
-﻿// Branch administration page
+// Branch administration page
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
